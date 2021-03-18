15f0b711262b86aa20ca71212f3f5888224bd7a7
senaybee <senay_berihu@outlook.com>
2021-03-18 16:13:39 -0500

    Delete redundant Home.dart file

--- just_ask/lib/screens/Home.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:just_ask/screens/QuestionBankList.dart';
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return QuestionBankList();
-  }
-}
--- just_ask/lib/screens/JustAsk.dart
+++ just_ask/lib/screens/JustAsk.dart
@@ -1,14 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:just_ask/screens/QuestionBankList.dart';
 import 'package:just_ask/screens/SignInOrRegister.dart';
 import 'package:provider/provider.dart';
-import 'Home.dart';
 
 //Top level widget that injects user state to rest of the widget tree
 class JustAsk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User currentUser = context.watch<User>();
-    return currentUser == null ? SignInOrRegister() : Home();
+    return currentUser == null ? SignInOrRegister() : QuestionBankList();
   }
 }
